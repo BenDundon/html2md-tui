a2c6802d8bf471114d6a3a481aef38a70cc7aa16
Ben Dundon <benjamin.dundon@protonmail.com>
2024-08-24 16:28:35 +1000

    feat(main): Add file reader framework
    Added a framework to read a file, which will be used to pipe into
    Johannes Kaufmann's converter.

--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+    "log"
+    "os"
+    "io"
+	md "github.com/JohannesKaufmann/html-to-markdown"
 )
 
 func main() {
-	fmt.Println("Hello flake")
+	converter := md.NewConverter("", true, nil)
+
+    html := readFile("someFile.txt") 
+    
+	markdown, err := converter.ConvertString(html)
+
+	if err != nil {
+  		log.Fatal(err)
+	}
+	
+	fmt.Println("md ->", markdown)
+}
+
+func readFile(fileName string) string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    // Write the file text somewhere else so we can close it
+    fileText, err := io.ReadAll(file)
+
+    if err != nil {
+    	log.Fatal(err)
+    }
+
+    // File *Must* be manually closed
+    file.Close()
+    
+	return string(fileText)
 }
